1b48cff414f3048535ffa273714269e116d6f559
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-06-10 09:35:42 -0700

    fix: task 3

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -3,7 +3,7 @@
 def print_matrix_integer(matrix=[[]]):
     for inner_list in matrix:
         for child_list in inner_list:
-            if child_list % 3 == 0:
+            if child_list == inner_list[-1]:
                 print("{:d}".format(child_list), end="")
             else:
                 print("{:d}".format(child_list), end=" ")
