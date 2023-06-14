9765b2d6f5ed58b3c95fe0515a907024ba247d50
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-06-14 08:37:31 -0700

    fix: task 11

--- 0x04-python-more_data_structures/11-multiply_list_map.py
+++ 0x04-python-more_data_structures/11-multiply_list_map.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def multiply_list_map(my_list=[], number=0):
     new_list = my_list.copy()
-    output = list(map(lambda num: num*number, new_list))
-    return output
+    return list(map(lambda num: num*number, new_list))
