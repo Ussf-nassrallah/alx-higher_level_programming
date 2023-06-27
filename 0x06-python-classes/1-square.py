9febd5b890aa76acddbfe1caafdc72196f4c2b7d
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-06-27 07:46:23 -0700

    done: task 1

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+""" define a square """
+
+
+class Square:
+    """ represent a square """
+
+    def __init__(self, size):
+        """ create a new square """
+        self.__size = size
