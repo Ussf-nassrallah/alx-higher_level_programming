48c995484d7c1259b04bcad925e987f02df2e6f0
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-02 05:59:52 -0700

    done: task 0

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+
+def add_integer(a, b=98):
+    """ Add function """
+    if type(a) not in [int, float]:
+        raise TypeError("a must be an integer")
+    elif type(b) not in [int, float]:
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
