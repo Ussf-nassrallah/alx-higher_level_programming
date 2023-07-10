f387b181604760fe8ab6b6e02ad4d33bc7604243
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-10 05:08:48 -0700

    done: task 3

---
+++ 0x0A-python-inheritance/3-is_kind_of_class.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" define my function """
+
+
+def is_kind_of_class(obj, a_class):
+    """ is_kind_of_class function """
+    output = isinstance(obj, a_class)
+    return output
