571dee3423e642c328f23d0122883e1b9d6867fd
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-10 07:15:15 -0700

    done: task 6

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+
+""" define BaseGeometry class """
+
+
+class BaseGeometry:
+    """ represent BaseGeometry class """
+    def area(self):
+        raise Exception("area() is not implemented")
