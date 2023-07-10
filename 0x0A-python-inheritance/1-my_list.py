333aad9b9324e34f556db02bbb26580f98f78160
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-10 04:36:56 -0700

    fix: task 1

--- 0x0A-python-inheritance/1-my_list.py
+++ 0x0A-python-inheritance/1-my_list.py
@@ -6,5 +6,7 @@
 class MyList(list):
     """ represent a MyList class """
     def print_sorted(self):
+        if not all(isinstance(ele, int) for ele in self):
+            raise TypeError("all the elements of the list must be of type int")
         output = sorted(self)
         print(output)
