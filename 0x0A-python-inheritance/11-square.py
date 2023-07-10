3b028241c63ffd75ae9a4810cf9c9d0d3573fba2
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-10 09:47:51 -0700

    done: task 11

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+
+""" define a Square class """
+
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ represent a Square class """
+    def __init__(self, size):
+        self.integer_validator("size", size)
+        super().__init__(size, size)
+        self.__size = size
