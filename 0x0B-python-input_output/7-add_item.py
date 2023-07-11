7785661906a820ec715667a2cd5710b8004aa7ba
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-11 08:08:59 -0700

    done: task 7

---
+++ 0x0B-python-input_output/7-add_item.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+import sys
+
+save_to_json_file = __import__('5-save_to_json_file').save_to_json_file
+load_from_json_file = __import__('6-load_from_json_file').load_from_json_file
+
+arguments = sys.argv[1:]
+
+try:
+    data = load_from_json_file("add_item.json")
+except FileNotFoundError:
+    data = []
+
+data.extend(arguments)
+save_to_json_file(data, "add_item.json")
