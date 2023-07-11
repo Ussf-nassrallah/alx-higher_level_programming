8dac5e43a7659d821ecd92835a34a8d86f71e701
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-11 02:38:32 -0700

    fix: task 1

--- 0x0B-python-input_output/1-write_file.py
+++ 0x0B-python-input_output/1-write_file.py
@@ -4,6 +4,7 @@
 
 
 def write_file(filename="", text=""):
+    """ returns the number of characters written """
     num_of_chars = 0
     with open(filename, 'w', encoding='utf-8') as file:
         num_of_chars = file.write(text)
