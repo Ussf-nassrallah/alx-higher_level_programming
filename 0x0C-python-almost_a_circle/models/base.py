b1c2b3b63179f98c54b82f5a16cb1eeccab564b7
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-17 17:05:06 +0100

    Base: to_json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -2,14 +2,26 @@
 
 """ define a Base class """
 
+import json
+
 
 class Base:
     """ represent a Base class """
     __nb_objects = 0
 
     def __init__(self, id=None):
+        """ init a new Base """
         if id is not None:
             self.id = id
         else:
             type(self).__nb_objects += 1
             self.id = self.__nb_objects
+
+    def to_json_string(list_dictionaries):
+        """
+          returns the JSON string representation of list_dictionaries
+        """
+        if list_dictionaries is None or list_dictionaries == "":
+            return "[]"
+        else:
+            return json.dumps(list_dictionaries)
