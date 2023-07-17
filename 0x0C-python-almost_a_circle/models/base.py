0eab4d2c00d3afc82ad0ec0ead436b05e30abd71
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-07-17 09:57:54 -0700

    fix: Base (to_dictionary)

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,4 +22,5 @@ class Base:
         """ returns the JSON string representation of list_dictionaries """
         if list_dictionaries is None or list_dictionaries == []:
             return "[]"
-        return json.dumps(list_dictionaries)
+        output = json.dumps(list_dictionaries)
+        return output
