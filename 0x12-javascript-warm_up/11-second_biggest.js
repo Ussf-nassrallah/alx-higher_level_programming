391965839549130c627eadad5be9c3d27faf78b1
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-11 07:14:15 -0700

    done: task 11

--- 0x12-javascript-warm_up/11-second_biggest.js
+++ 0x12-javascript-warm_up/11-second_biggest.js
@@ -2,7 +2,7 @@
 // script that searches the second biggest integer in the list of arguments.
 const { argv } = require('process');
 if (argv.length <= 3) {
-    console.log('0');
+  console.log('0');
 } else {
   const data = argv.slice(2).map(Number);
   const arg = data.sort(function (a, b) { return b - a; })[1];
