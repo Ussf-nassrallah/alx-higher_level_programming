4470cd90fcb83f89d36cd7b04e824b3ada2b9b37
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-11 09:46:16 +0100

    done: task 2

---
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -0,0 +1,12 @@
+#!/usr/bin/node
+// script that prints a message depending of the number of arguments passed
+
+import { argv } from "node:process";
+
+if (argv.length > 1) {
+    console.log("Arguments found");
+} else if (argv.length == 1) {
+    console.log("Argument found")
+} else {
+    console.log("No argument");
+}
