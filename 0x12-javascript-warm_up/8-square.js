af5f24072d3315de069369d487385ed2ac65fd8f
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-11 13:08:27 +0100

    fix: task 8

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,14 @@
 #!/usr/bin/node
 // script that prints a square
 const { argv } = require('process');
-if (isNaN(argv[2]) || argv[2] === undefined) {
-  console.log('Missing size');
-} else {
-  const size = parseInt(argv[2]);
-  for (let i = 0; i < size; i++) {
-    console.log('X'.repeat(size));
+try{
+	if(isNaN(argv[2])) throw "z";
+  for(let i = 0; i < parseInt(argv[2]); i++){
+    for(let j = 0; j < parseInt(argv[2]); j++){
+      process.stdout.write("X")
+    }
+    process.stdout.write("\n")    
   }
+} catch(err){
+  console.log("Missing size")
 }
