031c98feceb77f687eae5e6f9527b88b0fd0b3a2
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-11 03:43:57 -0700

    done: task 6

---
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -0,0 +1,7 @@
+#!/usr/bin/node
+// script that prints 3 lines
+const data = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
+const n = data.length;
+for (let index = 0; index < n; index++) {
+  console.log(data[index]);
+}
