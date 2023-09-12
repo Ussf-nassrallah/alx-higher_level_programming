ce4d20415a045549f56d1c084cb895c54362a179
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-12 04:23:41 -0700

    task 6 (done)

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,26 @@
+#!/usr/bin/node
+// class Square that defines a square and inherits from Rectangle
+const Rectangle = require('./4-rectangle');
+
+class Square extends Rectangle {
+  constructor (size) {
+    super(size, size);
+    this.size = size;
+  }
+
+  charPrint (c) {
+    let char;
+
+    if (!c) {
+      char = 'X';
+    } else {
+      char = c;
+    }
+
+    for (let i = 0; i < this.size; i++) {
+      console.log(char.repeat(this.size));
+    }
+  }
+}
+
+module.exports = Square;
