c0c78b7dfaa19e50a151b59caec35ac2fab95ef3
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-12 05:11:36 -0700

    fix: semistandard

--- 0x13-javascript_objects_scopes_closures/8-esrever.js
+++ 0x13-javascript_objects_scopes_closures/8-esrever.js
@@ -1,11 +1,11 @@
 #!/usr/bin/node
 // function that returns the reversed version of a list
 exports.esrever = function (list) {
-  let output = [];
+  const output = [];
   let index = list.length - 1;
   while (index >= 0) {
     output.push(list[index]);
     index--;
   }
   return output;
-}
+};
