ca88ba70d17422b489cc66a10c577a0b5fb2086a
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-12 03:42:01 -0700

    add: print/double/rotate methods to Rectangle

---
+++ 0x13-javascript_objects_scopes_closures/4-rectangle.js
@@ -0,0 +1,30 @@
+#!/usr/bin/node
+
+// class Rectangle that defines a rectangle
+class Rectangle {
+  constructor (w, h) {
+    if ((w > 0) && (h > 0)) {
+      this.width = w;
+      this.height = h;
+    }
+  }
+
+  print () {
+    for (let i = 0; i < this.height; i++) {
+      console.log('X'.repeat(this.width));
+    }
+  }
+
+  double () {
+    this.width *= 2;
+    this.height *= 2;
+  }
+
+  rotate () {
+    const helper = this.width;
+    this.width = this.height;
+    this.height = helper;
+  }
+}
+
+module.exports = Rectangle;
