73690c68a79bfc5689231cd57136c17d85f7c441
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-13 04:25:58 -0700

    done: task 101 done

--- 0x13-javascript_objects_scopes_closures/101-sorted.js
+++ 0x13-javascript_objects_scopes_closures/101-sorted.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 // script that imports a dictionary of occurrences
 // by user id and computes a dictionary of user ids by occurrence.
-const dict = require('./101-data').dict;
+const { dict } = require('./101-data');
 const newObj = {};
 
-Object.keys(dict).map((key) => {
+Object.keys(dict).forEach(function (key) {
   if (newObj[dict[key]] === undefined) {
     newObj[dict[key]] = [];
   }
