e38714332ddf34c5da6fcf8ef8934f1f4bbe7fad
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-13 09:56:48 +0100

    fix: task 103

--- 0x12-javascript-warm_up/103-object_fct.js
+++ 0x12-javascript-warm_up/103-object_fct.js
@@ -2,11 +2,14 @@
 const myObject = {
   type: 'object',
   value: 12,
-  incr: function () {
-    this.value += 1;
-  }
 };
+
 console.log(myObject);
+
+myObject.incr = function () {
+  this.value += 1;
+};
+
 myObject.incr();
 console.log(myObject);
 myObject.incr();
