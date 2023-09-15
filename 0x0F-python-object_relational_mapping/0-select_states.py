2f934653dc56026a17347689ea108b0faa94a529
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-15 03:37:46 -0700

    fix: pycodestyle

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -7,7 +7,13 @@ username = sys.argv[1]
 password = sys.argv[2]
 dbName = sys.argv[3]
 
-db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=dbName)
+db = MySQLdb.connect(
+    host='localhost',
+    port=3306,
+    user=username,
+    passwd=password,
+    db=dbName
+)
 cur = db.cursor()
 cur.execute('SELECT * FROM states ORDER BY states.id')
 
