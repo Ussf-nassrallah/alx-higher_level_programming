a629733366e1b4349e0b629d35bcbe0c4f467895
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-15 04:08:17 -0700

    done: task 2

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -1,23 +1,35 @@
 #!/usr/bin/python3
 
+"""
+Write a script that takes in an argument and displays all
+values in the states table of hbtn_0e_0_usa
+where name matches the argument
+"""
+
 import MySQLdb
 import sys
 
-username = sys.argv[1]
-password = sys.argv[2]
-dbName = sys.argv[3]
-user_input = sys.argv[4]
+if __name__ == "__main__":
+    username = sys.argv[1]
+    password = sys.argv[2]
+    dbName = sys.argv[3]
+    user_input = sys.argv[4]
 
-db = MySQLdb.connect(host='localhost', port=3306, user=username, passwd=password, db=dbName)
-cur = db.cursor()
+    db = MySQLdb.connect(
+        host='localhost',
+        port=3306,
+        user=username,
+        passwd=password,
+        db=dbName
+    )
+    cur = db.cursor()
 
-q = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+    q = "SELECT * FROM states WHERE name = %s ORDER BY states.id"
+    cur.execute(q, (user_input,))
 
-cur.execute(q, (user_input,))
+    states = cur.fetchall()
+    for state in states:
+        print(state)
 
-states = cur.fetchall()
-for state in states:
-    print(state)
-
-cur.close()
-db.close()
+    cur.close()
+    db.close()
