6f94897788b2a399a2867c10e8d05143263255ee
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-29 02:32:33 -0700

    displays the size of the body

--- 0x10-python-network_0/0-body_size.sh
+++ 0x10-python-network_0/0-body_size.sh
@@ -1,7 +1,3 @@
 #!/bin/bash
-# Bash script that takes in a URL, sends a request to that URL,
-#  and displays the size of the body of the response
-
-url="$1"
-key="Content-Length:"
-curl -sI "$url" | grep "$key" | cut -d " " -f 2
+# Bash script that displays the size of the body of the response
+url="$1" key="Content-Length:"; curl -sI "$url" | grep "$key" | cut -d " " -f 2
