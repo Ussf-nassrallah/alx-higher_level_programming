fc9b2b1344b82ae9681a275ea395f873e9def979
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-09-29 02:22:22 -0700

    displays the size of the body

---
+++ 0x10-python-network_0/0-body_size.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+# Bash script that takes in a URL, sends a request to that URL,
+#  and displays the size of the body of the response
+
+url="$1"
+key="Content-Length:"
+curl -sI "$url" | grep "$key" | cut -d " " -f 2
