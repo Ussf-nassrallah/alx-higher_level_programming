800f40a7275592b7fd104bc6954f6e6b01840477
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-10-01 11:38:25 +0100

    script that fetche url using urllib

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,12 +2,19 @@
 """ script that fetches https://alx-intranet.hbtn.io/status """
 import urllib.request
 
-base_url = "https://alx-intranet.hbtn.io/status"
-with urllib.request.urlopen(base_url) as response:
-    data = response.read()
 
-# print output
-print("Body response:")
-print(f"    - type: {type(data)}")
-print(f"    - content: {data}")
-print(f"    - utf8 content: {data.decode()}")
+def fetch_url():
+    """ fetch url and display the response """
+    base_url = "https://alx-intranet.hbtn.io/status"
+
+    with urllib.request.urlopen(base_url) as response:
+        data = response.read()
+
+    # print output
+    print("Body response:")
+    print(f"    - type: {type(data)}")
+    print(f"    - content: {data}")
+    print(f"    - utf8 content: {data.decode()}")
+
+if __name__ == "__main__":
+    fetch_url()
