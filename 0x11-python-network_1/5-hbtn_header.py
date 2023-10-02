1d3fa03ad3e5498beaf34f6f5863031e1777f908
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-10-02 02:35:14 -0700

    fetch url and display X-Request-Id in the response

---
+++ 0x11-python-network_1/5-hbtn_header.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+script that takes in a URL, sends a request to the URL and displays
+ the value of the variable X-Request-Id in the response header
+"""
+
+import sys
+import requests
+
+
+def fetch_url():
+    base_url = sys.argv[1]
+    res = requests.get(base_url)
+    headers = res.headers
+    print(headers['X-Request-Id'])
+
+
+if __name__ == "__main__":
+    fetch_url()
