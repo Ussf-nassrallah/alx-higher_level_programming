1010d130400d6881da223775b959353a634d7907
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-10-24 04:03:01 -0700

    node js: read data from file

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -7,10 +7,10 @@ const file = process.argv[2];
 // use the fs.readFile method to read the file
 // The content of the file must be read in utf-8
 fs.readFile(file, 'utf-8', function (data, error) {
-    // If an error occurred during the reading, print the error object
-    if (error) {
-        console.log(error);
-    } else {
-        console.log(data);
-    }
+  // If an error occurred during the reading, print the error object
+  if (error) {
+    console.log(error);
+  } else {
+    console.log(data);
+  }
 });
