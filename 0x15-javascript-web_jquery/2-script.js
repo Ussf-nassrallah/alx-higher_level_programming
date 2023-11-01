5e86d8e2ddaf4b5a922e1dab923b1741c32aa08d
Ussf-nassrallah <nassrallah.dev@gmail.com>
2023-11-01 13:10:37 +0100

    update header txt color to #ff0000 when the user click on the Tag div#red_header

---
+++ 0x15-javascript-web_jquery/2-script.js
@@ -0,0 +1,11 @@
+/*
+script that updates the text color of the <header>
+  element to red (#FF0000) when the user
+  clicks on the tag DIV#red_header
+*/
+
+$(document).ready(function () {
+  $('#red_header').click(function () {
+    $('header').css('color', '#FF0000');
+  });
+});
